4de4f5800c56de6223411aa6c84d90b9b172ab59
Ashlin Rofina <ashlinrofina@gmail.com>
2025-09-17 21:38:16 +0300

    add modification to index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob'// Write your solution in this file! 
+
+function upperCaseCustomerName () { 
+    customerName = customerName.toUpperCase('bob');
+}  
+
+function setBestCustomer() { 
+   window.bestCustomer = 'not bob'
+}
+ 
+function overwriteBestCustomer() { 
+    window.bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = "sylvia" 
+ 
+function changeLeastFavoriteCustomer() { 
+    leastFavoriteCustomer= "dario" 
+} 
+
